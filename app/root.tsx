72cae561517483fb6b4be01f2abca9e4b21324fe
John Cornyn <JMIHC@users.noreply.github.com>
2025-10-12 19:52:57 -0700

    test meta tag

--- app/root.tsx
+++ app/root.tsx
@@ -58,6 +58,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="google-site-verification" content="EtTi8mMNmnmGlhsRg0z6Ijcix1geFHAxwVgnMygJa-I" />
         <Meta />
         <Links />
         <script
